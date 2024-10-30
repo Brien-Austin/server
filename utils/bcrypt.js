eb41085f7d3c2973b9a63c6a233652c85f4c2cfa
Brien-Austin <brienaustinclayton@gmail.com>
2024-10-30 21:53:43 +0530

    bug fix

--- utils/bcrypt.js
+++ utils/bcrypt.js
@@ -1,4 +1,4 @@
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const hashValue = (value, saltRounds) => bcrypt.hash(value, saltRounds || 10);
 
 const compareValue = async (value, hash) =>
