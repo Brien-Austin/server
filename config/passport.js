059725ec33d58de1a13bba77b421b4d2cab4abc0
Brien-Austin <brienaustinclayton@gmail.com>
2024-11-01 09:42:57 +0530

    changed passport js

--- config/passport.js
+++ config/passport.js
@@ -21,7 +21,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/api/v1/auth/user/google/callback",
+      callbackURL: `${process.env.BACKEND_URL}/api/v1/auth/user/google/callback`,
       scope: ["profile", "email"],
     },
     async (accessToken, refreshToken, profile, done) => {
